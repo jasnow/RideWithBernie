a6a31a50ced06966658b7d265af93b4b1bfaec7d
Al Snow <jasnow@hotmail.com>
2019-04-28 10:06:01 -0400

    Fixed zeitwerk error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # forgery protection taken from:
   # http://stackoverflow.com/questions/14734243/
-  after_filter :set_csrf_cookie_for_ng
+  after_action :set_csrf_cookie_for_ng
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
